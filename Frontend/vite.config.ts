20e513c991baa8bc0f10187c2658b72dec8ae25a
Syrins <67106364+Syrins@users.noreply.github.com>
2025-11-26 19:21:46 +0300

    Add files via upload

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,23 +3,33 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    open: true,
-    strictPort: true,
-  },
-  plugins: [
-    react(),
-    ...(mode === "development" ? [componentTagger()] : []),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: "::",
+      port: 8080,
+      open: true,
+      strictPort: true,
+      proxy: mode === "development"
+        ? {
+            "/health": {
+              target: "https://api.cors.syrins.tech",
+              changeOrigin: true,
+            },
+          }
+        : undefined,
     },
-  },
-  esbuild: {
-    jsx: "automatic",
-  },
-}));
+    plugins: [
+      react(),
+      ...(mode === "development" ? [componentTagger()] : []),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    esbuild: {
+      jsx: "automatic",
+    },
+  };
+});
